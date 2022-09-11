a388e15ed7d76ccd542a9e8db295c4e34d5f7e5f
Juno99 <83945722+JunoJuno99@users.noreply.github.com>
2022-09-11 10:56:02 +0900

    Dom을 이용한 HTML 객체 제어

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,13 @@
 "use strict";
 
-console.log("hi");
-console.log("bye");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("button");
+const login = () => {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+};
+
+loginBtn.addEventListener("click", login);
