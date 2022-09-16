ee1a49c54c8e08075fdca93b93947403930e3866
Juno99 <83945722+JunoJuno99@users.noreply.github.com>
2022-09-16 20:32:12 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -1,15 +1,21 @@
 "use strict";
 
+const fs = require("fs").promises; // 파일 시스템 로드
+
 class UserStorage {
   // # -> private 변수 처리
-  static #users = {
-    id: ["juno"],
-    password: ["1234"],
-    name: ["준호"],
-  };
+  // private 변수 혹은 메소드는 항상 클래스 최상단에 위치하는것이 암묵적 룰
+  static #getUserInfo(users, id) {
+    const idx = users.id.indexOf(id);
+    const userInfo = Object.keys(users).reduce((userData, info) => {
+      userData[info] = users[info][idx];
+      return userData;
+    }, {});
+    return userInfo;
+  }
 
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const usersData = fields.reduce((usersData, field) => {
       if (users.hasOwnProperty(field)) {
         usersData[field] = users[field];
@@ -19,18 +25,17 @@ class UserStorage {
     return usersData;
   }
 
-  static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const userInfo = Object.keys(users).reduce((userData, info) => {
-      userData[info] = users[info][idx];
-      return userData;
-    }, {});
-    return userInfo;
+  static async getUserInfo(id) {
+    try {
+      const users = JSON.parse(await fs.readFile("./src/databases/users.json"));
+      return this.#getUserInfo(users, id);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.name.push(userInfo.name);
     users.id.push(userInfo.id);
     users.password.push(userInfo.password);
